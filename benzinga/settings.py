e5488eaaa85be8476e80bc1b0461a64c478ff2a6
Wenbin Wu <wenbin@fyndiq.se>
2014-08-12 14:55:53 -0400

    add urls

--- benzinga/settings.py
+++ benzinga/settings.py
@@ -81,7 +81,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
 )
+
+from decimal import Decimal
+INIT_CACHE = Decimal('100000.00')
--- benzinga/urls.py
+++ benzinga/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = patterns('',
     url(r'^', include('portfolio.urls')),
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

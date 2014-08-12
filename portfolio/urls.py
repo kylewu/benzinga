138316bf8f8c23f34950f567ea1a591c280e1f89
Wenbin Wu <wenbin@fyndiq.se>
2014-08-12 14:55:53 -0400

    add urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+
+
+STOCK_PATTERN = r'(?P<symbol>[a-zA-Z]+)/'
+QUANTITY_PATTERN = r'(?P<quantity>\d+)/'
+PRICE_PATTERN = r'(?P<price>[0-9.]+)/'
 
 
 urlpatterns = patterns('portfolio.views',
     url(r'^$', 'index'),
     url(r'^login/$', 'login'),
     url(r'^logout/$', 'logout'),
+    url(r'^reset/$', 'reset'),
+
+    url(r'^buy/' + STOCK_PATTERN + QUANTITY_PATTERN + PRICE_PATTERN + '$', 'buy'),
+    url(r'^sell/' + STOCK_PATTERN + QUANTITY_PATTERN + PRICE_PATTERN + '$', 'sell'),
 )
